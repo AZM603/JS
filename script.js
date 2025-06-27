ea8731630485723fd4c927a4baba2284663760f3
AZM603 <azm98098@gmail.com>
2025-06-27 16:39:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let a = prompt('your name')
+console.log(a);
+
+let w = prompt('your sunname')
+console.log(w);
+
+let b = prompt('your age')
+console.log(b);
+
+let e = prompt('your hobby')
+console.log(e);
+
+
+let f = prompt('fhere are you from')
+console.log(f);
